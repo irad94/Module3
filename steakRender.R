90beae1b264a14b1fd41bad8bb2923b10d1082a7
irad94 <irad.olriv@gmail.com>
2019-08-11 17:48:53 -0500

    added new steak article params

---
+++ steakRender.R
@@ -0,0 +1,32 @@
+R code for the render function
+
+rmarkdown::render("steakArticleParams.Rmd",
+                  params = list(region = "Mountain"))
+
+
+R code to create the custom function
+
+
+render_report <-function(regionvar){
+  template <-"steakArticleParams.Rmd"
+  outfile <-sprintf("steakArticle_%s.html",regionvar)
+  parameters <-list(region = regionvar)
+  rmarkdown::render(template,
+                    output_file=outfile,
+                    params=parameters)
+  invisible(TRUE)
+}
+
+render_report("Pacific")
+
+
+R code to use the custom function with purrr
+
+
+library(purrr)
+params_list <- list(list("East North Central",
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list,render_report)
